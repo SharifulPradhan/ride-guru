de505486aea9fe8166e828017e5f0212cf169651
WebBygger <sharifulpradhanhridoy@gmail.com>
2021-03-20 18:54:57 +0600

    sign in with email password udpate

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,6 +7,7 @@ import { faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons'
 import { Button } from 'react-bootstrap';
 import { useContext } from 'react';
 import { UserContext } from '../../App';
+import { Link } from 'react-router-dom';
 
 
 if (!firebase.apps.length) {
@@ -20,6 +21,8 @@ const Login = () => {
   const googleProvider = new firebase.auth.GoogleAuthProvider();
   const facebookProvider = new firebase.auth.FacebookAuthProvider();
 
+
+  const [registeredUser, setRegisteredUser] = useState(false);
   // declare a State for store user data
   const [user, setUser] = useState({
     isSignIn: false,
@@ -32,8 +35,8 @@ const Login = () => {
   })
   const [loggedInUser, setLoggedInUser] = useContext(UserContext);
 
+  // Google Firebase Authentication
   const handleGoogleSignin = () => {
-    // Google Firebase Authentication
     firebase.auth().signInWithPopup(googleProvider)
       .then(res => {
         const { displayName, email, photoURL } = res.user;
@@ -64,6 +67,7 @@ const Login = () => {
           photo: photoURL
         }
         setUser(signedInUser);
+        setLoggedInUser(signedInUser);
       })
       .catch(err => {
         console.log(err.message);
@@ -88,7 +92,7 @@ const Login = () => {
   }
 
   const handleSubmit = (e) => {
-    if(user.email && user.password){
+    if(!registeredUser && user.email && user.password){
       firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
     .then( res => {
       const newUserInfo = {...user};
@@ -101,25 +105,50 @@ const Login = () => {
       newUserInfo.error = error.message;
       newUserInfo.success = false;
       setUser(newUserInfo);
-      console.log(newUserInfo);
     })
   }
-  e.preventDefault();
+  if(registeredUser && user.email && user.password){
+    firebase.auth().signInWithEmailAndPassword(user.email, user.password)
+  .then( res => {
+      const newUserInfo = {...user};
+      newUserInfo.error = '';
+      newUserInfo.success = true;
+      setUser(newUserInfo);
+  })
+  .catch((error) => {
+    const newUserInfo = {...user};
+    newUserInfo.error = error.message;
+    newUserInfo.success = false;
+    setUser(newUserInfo);
+  });
   }
 
+  e.preventDefault();
+  }
   return (
     <div className="container border shadow rounded text-center d-flex flex-column justify-content-center align-items-center w-50">
       <form onSubmit={handleSubmit} className="form-group d-flex flex-column w-50">
-        <h1>Create an account</h1>
+        <h1> { registeredUser ? 'Login' : 'Create an account'}
+        </h1>
         <p style={{color:"red", fontWeight:'700'}}>{user.error}</p>
         {
-        user.success && <p style={{color:"green", fontWeight:'700'}}>Account successfully created</p>
+        user.success && <p style={{color:"green", fontWeight:'700'}}>{registeredUser ? "login Successfully" : "Account successfully created"}</p>
         }
-        <input type="text" className="form-control mt-5" name="name" onBlur={handleBlur}  placeholder="Name" required/>
+        {
+          !registeredUser && <input type="text" className="form-control mt-5" name="name" onBlur={handleBlur}  placeholder="Name" required/>
+        }
         <input type="email" className="form-control mt-5" name="email" onBlur={handleBlur} placeholder="Email" required/>
         <input type="password" className="form-control mt-5" name="password" onBlur={handleBlur} placeholder="Password" required/>
-        <input type="submit" className="btn btn-primary mt-5" value="Create an account" />
-        <p>Already have an account? <a href="login">Login</a></p>
+        {
+          registeredUser 
+          ? <input type="submit" className="btn btn-primary mt-5" value="Login" />
+          : <input type="submit" className="btn btn-primary mt-5" value="Create an account" />
+        }
+        {
+          registeredUser
+          ?<p>Don't have an account? <Link to="/login" onClick={() => setRegisteredUser(!registeredUser)}>Create an account</Link></p>
+          :<p>Already have an account? <Link to="/login" onClick={() => setRegisteredUser(!registeredUser)}>Login</Link></p>
+        }
       </form>
       <h1>or</h1>
       <Button size="lg" onClick={handleFBSignin}><FontAwesomeIcon icon={faFacebook} /> Sing in with Facebook</Button>
